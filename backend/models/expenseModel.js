8f0bc1337e6745958f58a11a99d306277098f141
KOUSTUBH <156505860+wurtz-codez@users.noreply.github.com>
2025-01-22 01:01:57 +0530

    Added Models in the database

---
+++ backend/models/expenseModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const expenseSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  done: {
+    type: Boolean,
+    default: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+}, {timestamps: true})
+
+export const Expense = mongoose.model("Expense", expenseSchema)
+
---
+++ backend/models/userModel.js
@@ -0,0 +1,18 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+export const User = mongoose.model("User", userSchema);
