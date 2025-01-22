f9279ab49820dc6f32f9088710d28bf1c05a6923
KOUSTUBH <156505860+wurtz-codez@users.noreply.github.com>
2025-01-23 00:12:39 +0530

    completed user controllers. Added login, register and log out functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -35,33 +35,59 @@ export const register = async (req, res) => {
   }
 }
 
-export const login = async(req, res) => {
+export const login = async (req, res) => {
   try {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
-    if(!email || !password){
+    if (!email || !password) {
       return res.status(400).json({
         message: `All fields are required.`,
         success: false
       });
     }
 
-   const user = await User.findOne({email});
-   if(!user) {
-     return res.status(400).json({
-      message: "Email does not exist.",
-      success: false     
-    })
-  }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "Email does not exist.",
+        success: false
+      })
+    }
 
-  const passwordMatch = await bcrypt.compare(password, user.password);
-  if(!passwordMatch) {
-    return res.status(400).json({
-      message: 'Incorrect Password.',
-      success: false
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(400).json({
+        message: 'Incorrect password.',
+        success: false
+      });
+    }
+    const tokenData = {
+      userId: user._id
+    }
+
+    const token = await jwt.sign(tokenData, process.env.SECRET_KEY, { expiresIn: '1d' });
+    return res.status(201).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }).json({
+      message: `Welcome back ${user.fullname}`,
+      user: {
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email
+      },
+      success: true
     })
+
+  } catch (error) {
+    console.error(error);
+
   }
-  
+}
+
+export const logout = async(req,res) => {
+  try {
+    return res.status(201).cookie("token", null, {maxAge: 0}).json({
+      message: `User logged out successfully.`,
+      success: true
+    })
   } catch (error) {
     console.error(error);
     
