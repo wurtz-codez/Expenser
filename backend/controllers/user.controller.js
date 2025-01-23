a33b2f9adfdba8f3a85a080b33f018743076f84a
KOUSTUBH <156505860+wurtz-codez@users.noreply.github.com>
2025-01-23 20:23:06 +0530

    fixed some bugs in routes folder

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { User } from "../models/userModel";
+import { User } from "../models/userModel.js";
 
 export const register = async (req, res) => {
   try {
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { login, register, logout } from '../controllers/user.controller';
+import { login, register, logout } from '../controllers/user.controller.js';
 
 const router = express.Router(); 
 
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/logout').post(logout);
+
+export default router;
