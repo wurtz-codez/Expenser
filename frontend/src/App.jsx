9d8aeface0238bb2d377566df43eacafd00daa59
Koustubh <koustubhpande021@gmail.com>
2025-01-27 11:41:43 +0530

    frontend - 1
    setting up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+
+const appRouter = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Home/>
+    },
+    {
+      path: '/login',
+      element: <Login/>
+    },
+    {
+      path: '/signup',
+      element: <SignUp/>
+    },
+
+  ]
+);
+
+
+function App() {
+  
+  return (
+    <>
+      <RouterProvider router = {appRouter}/>
+    </>
+  )
+}
+
+export default App
